759d38a955a475f59cba601e8530b3200d7ddf07
Senthil kumar K <nightrider.cbe@gmail.com>
2019-04-01 11:51:27 +0530

    Commit

---
+++ Internalization/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Internalization
+//
+//  Created by appledeveloper on 01/04/19.
+//  Copyright © 2019 Senthilkumar K. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Internalization/ViewController.m
@@ -0,0 +1,35 @@
+//
+//  ViewController.m
+//  Internalization
+//
+//  Created by appledeveloper on 01/04/19.
+//  Copyright © 2019 Senthilkumar K. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+#pragma mark - English Button Action
+- (IBAction)englishAction:(id)sender {
+    
+    
+}
+
+#pragma mark - Spanish Button Action
+- (IBAction)spanishAction:(id)sender {
+    
+    
+}
+
+
+@end
